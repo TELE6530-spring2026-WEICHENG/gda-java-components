6e29eac0a71ef26a09aefea45572bba4f0500002
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2026-04-19 22:18:35 -0400

    set enableSystemPerformance = True

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -55,7 +55,7 @@ public class SystemPerformanceManager {
 
 		this.locationID =
 			ConfigUtil.getInstance().getProperty(
-				ConfigConst.GATEWAY_DEVICE, ConfigConst.LOCATION_ID_PROP, ConfigConst.NOT_SET);
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY, ConfigConst.NOT_SET);
 
 
 		if (this.pollRate <= 0) {
